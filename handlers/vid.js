507f6e70c9688efec6697d26fcaa6af76c8bdf1a
RSchmeitz <48622404+RSchmeitz@users.noreply.github.com>
2025-03-18 00:01:42 +0100

    Update vid.js

--- handlers/vid.js
+++ handlers/vid.js
@@ -3,10 +3,12 @@ AFRAME.registerComponent('videohandler', {
         var marker = this.el;
         this.vid1 = document.querySelector("#vid1");
         this.vid2 = document.querySelector("#vid2");
+        this.vid3 = document.querySelector("#vid3");
+        this.vid4 = document.querySelector("#vid4");
         this.currentVid = this.vid1;
         this.videoTitle = document.querySelector("#videoTitle");
         this.videoNumber = 1;
-        this.videoTitles = ["Video 1: Environment demo", "Video 2: RL Agent Playthrough"]; // Add video titles
+        this.videoTitles = ["Video 1: Environment demo", "Video 2: RL Agent Playthrough", "Video 3: RL Agent Playthrough", "Video 4: RL Agent Playthrough"];
 
         var videoEl = marker.querySelector('a-video'); // Select the video entity
 
@@ -45,8 +47,16 @@ AFRAME.registerComponent('videohandler', {
         };
 
         const switchToNextVideo = () => {
-            this.videoNumber = this.videoNumber === 2 ? 1 : 2;
-            this.currentVid = this.videoNumber === 1 ? this.vid1 : this.vid2;
+            // Update to cycle through all 4 videos
+            this.videoNumber = (this.videoNumber % 4) + 1;
+            
+            // Set current video based on video number
+            switch(this.videoNumber) {
+                case 1: this.currentVid = this.vid1; break;
+                case 2: this.currentVid = this.vid2; break;
+                case 3: this.currentVid = this.vid3; break;
+                case 4: this.currentVid = this.vid4; break;
+            }
 
             // Update UI
             this.videoTitle.setAttribute('value', this.videoTitles[this.videoNumber - 1]);
