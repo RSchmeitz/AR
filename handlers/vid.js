ddf9640cc57e8003dd8a71b64db2dd2b363e9639
Schmeitz <r.schmeitz@tue.nl>
2025-07-02 16:54:53 +0200

    test version for second poster

--- handlers/vid.js
+++ handlers/vid.js
@@ -44,19 +44,43 @@ AFRAME.registerComponent('scale-on-pinch', {
 });
 
 AFRAME.registerComponent('videohandler', {
+    schema: {
+        markerId: {type: 'string', default: 'p'}
+    },
     init: function () {
         var marker = this.el;
+        var markerId = this.data.markerId;
+        
         this.vid1 = document.querySelector("#vid1");
         this.vid2 = document.querySelector("#vid2");
         this.vid3 = document.querySelector("#vid3");
         this.vid4 = document.querySelector("#vid4");
-        this.currentVid = this.vid1;
-        this.videoTitle = document.querySelector("#videoTitle");
+        
+        // Set current video based on marker
+        switch(markerId) {
+            case 'r': this.currentVid = this.vid1; break;
+            case 'l': this.currentVid = this.vid2; break;
+            default: this.currentVid = this.vid1; break;
+        }
+        
+        this.videoTitle = document.querySelector("#videoTitle_" + markerId);
         this.videoNumber = 1;
-        this.videoTitles = ["Video 1: Environment demo", "Video 2: RL Agent Playthrough", "Video 3: RL Agent Playthrough", "Video 4: RL Agent Playthrough"];
+        
+        // Set titles based on marker
+        switch(markerId) {
+            case 'r':
+                this.videoTitles = ["Right Marker: Video 1"];
+                break;
+            case 'l':
+                this.videoTitles = ["Left Marker: Video 2"];
+                break;
+            default:
+                this.videoTitles = ["Video 1: Environment demo", "Video 2: RL Agent Playthrough", 
+                                  "Video 3: RL Agent Playthrough", "Video 4: RL Agent Playthrough"];
+                break;
+        }
 
-        var videoEl = marker.querySelector('a-video'); // Select the video entity
-
+        var videoEl = marker.querySelector('a-video');
         let markerVisible = false;
         let lastMarkerTime = 0;
         const MARKER_TIMEOUT = 300; // Time in ms before considering marker lost
@@ -120,30 +144,29 @@ AFRAME.registerComponent('videohandler', {
         };
 
         const switchToNextVideo = () => {
-            // Update to cycle through all 4 videos
-            this.videoNumber = (this.videoNumber % 4) + 1;
-            
-            // Set current video based on video number
-            switch(this.videoNumber) {
-                case 1: this.currentVid = this.vid1; break;
-                case 2: this.currentVid = this.vid2; break;
-                case 3: this.currentVid = this.vid3; break;
-                case 4: this.currentVid = this.vid4; break;
-            }
+            // Only allow video switching for p_marker
+            if (markerId === 'p') {
+                this.videoNumber = (this.videoNumber % 4) + 1;
+                
+                switch(this.videoNumber) {
+                    case 1: this.currentVid = this.vid1; break;
+                    case 2: this.currentVid = this.vid2; break;
+                    case 3: this.currentVid = this.vid3; break;
+                    case 4: this.currentVid = this.vid4; break;
+                }
 
-            // Update UI
-            this.videoTitle.setAttribute('value', this.videoTitles[this.videoNumber - 1]);
-            videoEl.setAttribute('material', 'src', '#vid' + this.videoNumber);
+                this.videoTitle.setAttribute('value', this.videoTitles[this.videoNumber - 1]);
+                videoEl.setAttribute('material', 'src', '#vid' + this.videoNumber);
 
-            // Restart and play the new video
-            this.currentVid.currentTime = 0;
-            this.currentVid.play();
-            isPlaying = true;
-            playPauseBtn.innerHTML = '⏸';
+                this.currentVid.currentTime = 0;
+                this.currentVid.play();
+                isPlaying = true;
+                playPauseBtn.innerHTML = '⏸';
 
-            if (!isARMode) {
-                normalVideo.src = this.currentVid.src;
-                normalVideo.play();
+                if (!isARMode) {
+                    normalVideo.src = this.currentVid.src;
+                    normalVideo.play();
+                }
             }
         };
 
